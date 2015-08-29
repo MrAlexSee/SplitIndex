12f067a7b9b52b52778f83df7a9561abcf9d22c5
MrAlexSee <a.cislak@tum.de>
2015-08-29 09:54:57 +0200

    comments

--- testing/run_and_compare.sh
+++ testing/run_and_compare.sh
@@ -2,13 +2,17 @@
 
 set -e
 
+# Input dictionary
 dictPath="/home/alex/Documents/Textual/English dict/english-dict_filtered.txt"
+# File with queries
 queriesPath="english_typos.txt"
 
-indexOutPath="search_ham.dat" # Must be the same as the output path of the index (params.hpp : outFilePath)
+# Must be the same as the output path of the index (params.hpp : outFilePath)
+indexOutPath="search_ham.dat"
+# This can be anything.
 naiveOutPath="out_naive.dat"
 
-# Make sure that both programs are set for the same k and min word length value.
+# Make sure that both programs are set for the same k and min word length value!
 
 # First we run the naive hamming.
 ./naive_hamming.py "$dictPath" "$queriesPath" "$naiveOutPath"
