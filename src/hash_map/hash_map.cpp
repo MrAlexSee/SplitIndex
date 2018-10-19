1fd9929bd46c598456c90e9bf02251d8c4d40d24
Aleksander Cisłak <alex.cis91@gmail.com>
2018-10-19 20:47:30 +0200

    Minor formatting fix.

--- src/hash_map/hash_map.cpp
+++ src/hash_map/hash_map.cpp
@@ -22,7 +22,7 @@ HashMap::HashMap(const std::function<size_t(const char *)> &calcEntrySizeBArg,
      nBuckets(nBucketsHint)
 {
     static_assert(bucketRehashFactor > 1.0f, "bucket rehash factor must be greater than 1.0");
-assert(maxLoadFactor > 0.0f);
+    assert(maxLoadFactor > 0.0f);
     assert(nBuckets > 0);
 
     hash = hash_functions::HashFunctions::getHashFunction(hashType);
