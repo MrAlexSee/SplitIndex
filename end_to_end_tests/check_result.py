623464911acaf04bfca996e05a52af1e0d3b05cc
Aleksander Cisłak <alex.cis91@gmail.com>
2018-11-06 18:31:28 +0100

    Added basic e2e tests.

---
+++ end_to_end_tests/check_result.py
@@ -0,0 +1,25 @@
+"""
+Checks whether the result (number of matches) from split index is the same as the desired value.
+"""
+
+import sys
+
+pInFile = "res.txt"
+
+def main():
+    with open(pInFile, "r") as f:
+        data = f.read()
+
+    lines = data.split("\n")
+    lines = [l for l in lines if l]
+
+    nMatches = int(lines[-2].split()[-1])
+    nExpected = int(sys.argv[1])
+
+    if nMatches == nExpected:
+        print "OK (#matches = {0})".format(nMatches)
+    else:
+        print "ERROR: {0} != {1}".format(nMatches, nExpected)
+
+if __name__ == "__main__":
+    main()
---
+++ end_to_end_tests/run_tests.sh
@@ -0,0 +1,29 @@
+outFile="res.txt"
+
+echo "1/4 Rebuilding"
+cd ..
+make rebuild
+
+echo "2/4 Copying"
+cp build/split_index end_to_end_tests/split_index
+cd end_to_end_tests
+
+echo "3/4 Running"
+
+# All index types for k=1.
+for iType in k1;
+do
+    # All hash types.
+    for hType in city farm farsh fnv1 fnv1a murmur3 sdbm spookyv2 superfast xxhash;
+    do
+        # Some selected maximum load factor values.
+        for maxLF in 0.5 1.0 1.5 2.25 2.5 3 4 5 6 10
+        do
+            ./split_index --index-type $iType --hash-type $hType --max-load-factor $maxLF -i dict_test.txt -I queries_test.txt --min-word-length 2 > $outFile
+            python check_result.py 3
+        done
+    done
+done
+
+echo "4/4 Teardown"
+rm -f split_index $outFile
