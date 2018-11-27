a82e729b8a66c5af77e52e93fc3a6fc38fd8e37e
Aleksander Cisłak <alex.cis91@gmail.com>
2018-11-27 20:02:16 +0100

    WIP: basic split index k construction.

--- src/index/split_index_k.hpp
+++ src/index/split_index_k.hpp
@@ -3,6 +3,8 @@
 
 #include "split_index.hpp"
 
+#include "../hash_map/hash_map_aligned.hpp"
+
 #ifndef SPLIT_INDEX_K_WHITEBOX
 #define SPLIT_INDEX_K_WHITEBOX
 #endif
@@ -11,7 +13,7 @@ namespace split_index
 {
 
 /** Split index for any k = 1, 2, 3. */
-template<int k>
+template<size_t k>
 class SplitIndexK : public SplitIndex
 {
 public:
@@ -21,23 +23,39 @@ public:
 
 protected:
     // We have 2 bits (4 positions) per word.
-    static constexpr const int maxK = 3;
+    static constexpr const size_t maxK = 3;
+
+    char *parts[k + 1];
+    size_t partSizes[k + 1];
 
     SPLIT_INDEX_K_WHITEBOX
 };
 
-template<int k>
+template<size_t k>
 SplitIndexK<k>::SplitIndexK(const std::unordered_set<std::string> &wordSet,
         hash_functions::HashFunctions::HashType hashType, float maxLoadFactor)
     :SplitIndex(wordSet)
 {
+    static_assert(k >= 1 and k <= maxK, "k must be between 1 and maxK");
 
+    auto calcEntrySizeB = std::bind(&SplitIndexK<k>::calcEntrySizeB, this, std::placeholders::_1);
+    const int nBucketsHint = std::max(1, static_cast<int>(nBucketsHintFactor * wordSet.size()));
+
+    hashMap = new hash_map::HashMapAligned(calcEntrySizeB, maxLoadFactor, nBucketsHint, hashType);
+
+    for (size_t i = 0; i < k + 1; ++i)
+    {
+        parts[i] = new char[maxWordSize];
+    }
 }
 
-template<int k>
+template<size_t k>
 SplitIndexK<k>::~SplitIndexK()
 {
-    
+    for (size_t i = 0; i < k + 1; ++i)
+    {
+        delete[] parts[i];
+    }
 }
 
 } // namespace split_index
