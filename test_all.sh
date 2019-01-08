d2ddb243e063f1eac7424fa165e24e8c37e21e43
Aleksander Cisłak <alex.cis91@gmail.com>
2019-01-08 09:14:23 +0100

    Updated testing script to process all index types and dictionaries.

--- test_all.sh
+++ test_all.sh
@@ -7,15 +7,18 @@ maxLF=3.0
 nIter=1
 
 # All index types.
-for iType in k1 k1comp;
+for iType in k1 k1comp k1comptriple k2 k3;
 do
     # All hash types.
     for hType in city farm farsh fnv1 fnv1a murmur3 sdbm spookyv2 superfast xxhash;
     do
-        # Selected dictionaries.
-        for dictFile in dict_foster dict_iamerican_insane;
+        # English dictionaries.
+        for dictFile in dict_english_foster dict_english_iamerican_insane;
         do
-            ./build/split_index --index-type $iType --hash-type $hType --max-load-factor $maxLF --iter $nIter -i ${inputDir}/${dictFile}.txt -I ${inputDir}/queries_misspellings.txt -o $outFile -d
+            ./build/split_index --index-type $iType --hash-type $hType --max-load-factor $maxLF --iter $nIter -i ${inputDir}/${dictFile}.txt -I ${inputDir}/queries_english_misspellings.txt -o $outFile -d
         done
+
+        # DNA dictionary.
+        ./build/split_index --index-type $iType --hash-type $hType --max-load-factor $maxLF --iter $nIter -i ${inputDir}/dict_dna_fruitfly.txt -I ${inputDir}/queries_dna_fruitfly.txt -o $outFile -d
     done
 done
